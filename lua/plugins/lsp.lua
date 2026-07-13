6501105c296890086456cfae4dd5a445bb738d31
Stivi Guranjaku <guranjakustivi@gmail.com>
2026-07-13 11:18:11 -0400

    lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,65 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.foldingRange = { dynamicRegistration = false, lineFoldingOnly = true }
 
+local hover_timers = {}
+local hover_delay_ms = 1000
+
+local function close_hover_timer(bufnr)
+  local timer = hover_timers[bufnr]
+  if timer then
+    timer:stop()
+    timer:close()
+    hover_timers[bufnr] = nil
+  end
+end
+
+local function schedule_hover(bufnr)
+  close_hover_timer(bufnr)
+
+  if vim.fn.mode() ~= "n" then
+    return
+  end
+
+  local win = vim.api.nvim_get_current_win()
+  local cursor = vim.api.nvim_win_get_cursor(win)
+  local symbol = vim.fn.expand("<cword>")
+  if symbol == "" then
+    return
+  end
+
+  local timer = vim.uv.new_timer()
+  hover_timers[bufnr] = timer
+
+  timer:start(hover_delay_ms, 0, function()
+    vim.schedule(function()
+      close_hover_timer(bufnr)
+
+      if not vim.api.nvim_buf_is_valid(bufnr) or not vim.api.nvim_win_is_valid(win) then
+        return
+      end
+
+      if vim.api.nvim_get_current_buf() ~= bufnr or vim.api.nvim_get_current_win() ~= win then
+        return
+      end
+
+      if vim.fn.mode() ~= "n" or vim.fn.pumvisible() == 1 then
+        return
+      end
+
+      local current_cursor = vim.api.nvim_win_get_cursor(win)
+      if current_cursor[1] ~= cursor[1] or current_cursor[2] ~= cursor[2] then
+        return
+      end
+
+      if vim.fn.expand("<cword>") ~= symbol then
+        return
+      end
+
+      vim.lsp.buf.hover()
+    end)
+  end)
+end
+
 vim.lsp.config("*", {
   capabilities = capabilities,
 })
@@ -19,6 +78,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     map("n", "gy", require("telescope.builtin").lsp_type_definitions, "Go to type definition")
     map("n", "gi", require("telescope.builtin").lsp_implementations, "Go to implementation")
     map("n", "gr", require("telescope.builtin").lsp_references, "Go to references")
+    map("n", "K", vim.lsp.buf.hover, "Hover")
     map("n", "P", vim.lsp.buf.hover, "Hover")
 
     map("n", "[g", vim.diagnostic.goto_prev, "Previous diagnostic")
@@ -36,6 +96,21 @@ vim.api.nvim_create_autocmd("LspAttach", {
       group = grp,
       callback = vim.lsp.buf.clear_references,
     })
+    vim.api.nvim_create_autocmd({ "CursorMoved", "BufEnter" }, {
+      buffer = bufnr,
+      group = grp,
+      callback = function()
+        schedule_hover(bufnr)
+      end,
+    })
+    vim.api.nvim_create_autocmd({ "CursorMovedI", "InsertEnter", "BufLeave", "WinLeave" }, {
+      buffer = bufnr,
+      group = grp,
+      callback = function()
+        close_hover_timer(bufnr)
+      end,
+    })
+    schedule_hover(bufnr)
 
     vim.api.nvim_buf_create_user_command(bufnr, "OR", function()
       vim.lsp.buf.code_action({
